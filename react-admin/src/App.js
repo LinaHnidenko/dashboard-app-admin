17a2038a59a2add33f6bff17606544c5f8f389e1
LinaHnidenko <hnidenko99@gmail.com>
2023-12-13 23:05:58 +0200

    create data and pages fields

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -4,11 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team/Team.jsx";
+import Contacts from "./scenes/contacts/Contacts.jsx";
+import Invoices from "./scenes/invoices/Invoices.jsx";
+import Form from "./scenes/form/Form.jsx";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -27,11 +27,11 @@ function App() {
           <main className="content">
             <Topbar />
             <Routes>
-              <Route path="/" element={<Dashboard />}></Route>
-              {/* <Route path="/team" element={<Team />}></Route> */}
-              {/* <Route path="/contacts" element={<Contacts />}></Route> */}
-              {/* <Route path="/invoices" element={<Invoices />}></Route> */}
-              {/* <Route path="/form" element={<Form />}></Route> */}
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />}></Route> */}
               {/* <Route path="/pie" element={<Pie />}></Route> */}
               {/* <Route path="/line" element={<Line />}></Route> */}
